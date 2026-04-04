7d3bd6131e05ae0e0437f5b14bd02ee7cb059bd0
NikhilGoral28 <nikhilgoral587@gmail.com>
2026-04-04 20:04:19 +0530

    data ingestion pipeline

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 ## Chicken Disease Classification
+
+
+## workflow
+
+1. Update config.yaml
+2. Update secrets.yaml [optional]
+3. Update paramas.yaml
+4. Update the entity
+5. Update the configuration manager in src config
+6. Update the components
+7. Update the pipeline
+8. Update the main.py
+9. update the dvc.yaml
--- main.py
+++ main.py
@@ -1,4 +1,15 @@
 from ccnClassifier import logger
+from ccnClassifier.pipeline.stage01_data_ingestion import DataIngestionTrainingPipeline
 
 
-logger.info("welcome to custom log")
+
+STAGE_NAME = "Data Ingestion stage"
+try:
+    logger.info(f">>> stage {STAGE_NAME} started <<<")
+    data_ingestion = DataIngestionTrainingPipeline()
+    data_ingestion.main()
+    logger.info(f">>>> stage {STAGE_NAME} completed <<<<\n\nx==========x")
+
+except Exception as e:
+    logger.exception(e)
+    raise e
